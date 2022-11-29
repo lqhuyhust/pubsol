10e0b16e68fe18825aca2357f7fa109260df311e
joomaio.tinhcv <cvtinh2000@gmail.com>
2022-11-29 12:04:54 +0700

    create basic sdm

---
+++ php_modules/config_sample.php
@@ -0,0 +1,13 @@
+<?php  defined('APP_PATH') or die('Invalid config');
+
+return [ 
+    'sitepath' => '',
+    'plugins' => ['starter'],
+    'theme' => 'demo',
+    'secrect' => 'sid',
+    'endpoints' => [
+    ],
+    'defaultEndpoint' => [
+        'fnc' => 'start.home.home'
+    ],
+];
