38583ba2fd18d09460f4c9a6dd14abdb8d2e7330
joomaio.tinhcv <cvtinh2000@gmail.com>
2022-11-30 10:23:24 +0700

    update user entity

--- php_modules/plugins/user/entities/UserEntity.php
+++ php_modules/plugins/user/entities/UserEntity.php
@@ -14,6 +14,54 @@ use SPT\User\SPT\UserEntity as Entity;
 
 class UserEntity extends Entity
 {
-    // protected $table = ''; //table name
-    // protected $pk = ''; //primary key
+    protected $table = '#__users';
+    protected $pk = 'id';
+
+    public function getFields()
+    {
+        return [
+                'id' => [
+                    'type' => 'int',
+                    'pk' => 1,
+                    'option' => 'unsigned',
+                    'extra' => 'auto_increment',
+                ],
+                'name' => [
+                    'type' => 'varchar',
+                    'limit' => 100,
+                ],
+                'username' => [
+                    'type' => 'varchar',
+                    'limit' => 100,
+                ],
+                'password' => [
+                    // 'validate' => ['md5'],
+                    'type' => 'varchar',
+                    'limit' => 255,
+                ],
+                'email' => [
+                    'type' => 'varchar',
+                    'limit' => 255,
+                ],
+                'status' => [
+                    'type' => 'tinyint',
+                ],
+                'created_at' => [
+                    'type' => 'datetime',
+                    'null' => 'YES',
+                ],
+                'created_by' => [
+                    'type' => 'int',
+                    'option' => 'unsigned',
+                ],
+                'modified_at' => [
+                    'type' => 'datetime',
+                    'null' => 'YES',
+                ],
+                'modified_by' => [
+                    'type' => 'int',
+                    'option' => 'unsigned',
+                ],
+        ];
+    }
 }
