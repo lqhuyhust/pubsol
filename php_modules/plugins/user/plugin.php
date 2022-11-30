3d940ed08073985daedeb22d523dbf4011b65f92
joomaio.tinhcv <cvtinh2000@gmail.com>
2022-11-30 10:04:33 +0700

    update register plugin user

--- php_modules/plugins/user/plugin.php
+++ php_modules/plugins/user/plugin.php
@@ -20,20 +20,51 @@ class plugin extends PluginAbstract
             // write your code here
             'viewmodels' => [
                 'alias' => [
-                    'App\plugins\user\viewmodels\UserVM' => 'UserVM',
+                    'App\plugins\user\viewmodels\AdminUsersVM' => 'AdminUsersVM',
+                    'App\plugins\user\viewmodels\AdminUserVM' => 'AdminUserVM',
                 ],
             ],
+            'models' => [
+                'alias' => [
+                    'App\plugins\user\models\UserModel' => 'UserModel',
+                ],
+            ],
+            'entity' => [],
+            'file' => [],
         ];
     }
 
     public function getInfo()
     {
         return [
-            'name' => 'starter',
+            'name' => 'user',
             'author' => 'Pham Minh',
             'version' =>  '0.1',
-            'description' => 'Sample Plugin'
+            'description' => 'User Plugin'
         ];
     }
 
+    public function loadFile(Container $container)
+    {
+        $container->set('file', new File());
+    }
+
+    public function loadEntity(Container $container)
+    {
+        $path = AppIns::path('plugin'). 'user/entities';
+        $namespace = 'App\plugins\user\entities';
+        $inners = Loader::findClass($path, $namespace);
+        foreach($inners as $class)
+        {
+            if(class_exists($class))
+            {
+                $entity = new $class($container->get('query'));
+                $entity->checkAvailability();
+                $container->share( $class, $entity, true);
+                $alias = explode('\\', $class);
+                $container->alias( $alias[count($alias) - 1], $class);
+            }
+            // else { debug this }
+        }
+    }
 }
