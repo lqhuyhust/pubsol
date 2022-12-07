abead146311e3bacc064626e7aa5839198b95ec4
Nguyễn Đắc Hùng <641043@sv.vnua.edu.vn>
2022-12-07 09:18:37 +0700

    fix button edit

--- php_modules/plugins/milestone/views/layouts/backend/request/list/row.php
+++ php_modules/plugins/milestone/views/layouts/backend/request/list/row.php
@@ -7,7 +7,17 @@
     <td><?php echo   $this->item['creator'] ?></td>
     <td><?php echo   $this->item['created_at'] ? date('m-d-Y', strtotime($this->item['created_at'])) : '';  ?></td>
     <td>
-        <a class="fs-4 me-1" href="<?php echo $this->link_form . '/' . $this->item['id']; ?>"><i class="fa-solid fa-pen-to-square"></i></a>
+        <a class="fs-4 me-1 show_data" 
+            href="#"
+            data-id="<?php echo  $this->item['id'] ?>" 
+            data-title="<?php echo  $this->item['title']  ?>" 
+            data-status="<?php echo   $this->item['status']?>"
+            data-note="<?php echo ($this->item['note']); ?>" 
+            data-bs-placement="top" 
+            data-bs-toggle="modal" 
+            data-bs-target="#exampleModalToggle">
+            <i class="fa-solid fa-pen-to-square"></i>
+        </a>
         <a data-id="<?php echo  $this->item['id'] ?>" style="color:#3b7ddd;" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete" class="delete fs-4 ps-1 border-0 bg-transparent button_delete_item"><i class="fa-solid fa-trash"></i></a>
     </td>
 </tr>
