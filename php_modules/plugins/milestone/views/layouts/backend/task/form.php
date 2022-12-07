4f1b73af668151c4ec0c6131d2720f1afee47870
Nguyễn Đắc Hùng <641043@sv.vnua.edu.vn>
2022-12-07 11:00:22 +0700

    fix form task

--- php_modules/plugins/milestone/views/layouts/backend/task/form.php
+++ php_modules/plugins/milestone/views/layouts/backend/task/form.php
@@ -1,36 +1,44 @@
-<div class="container-fluid align-items-center row justify-content-center mx-auto pt-3">
-    <div class="card shadow-none p-0 col-lg-12">
-        <div class="card-body">
+<div class="modal fade" id="exampleModalToggle" aria-labelledby="exampleModalToggleLabel" tabindex="-1" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog modal-dialog-centered " style="max-width: 600px;">
+        <div class="modal-content container px-5 pt-5">
             <?php echo $this->render('message'); ?>
-            <form action="<?php echo $this->link_form . '/' . $this->id ?>" method="post">
+            <form action="" method="post" id="form_task">
                 <div class="row g-3 align-items-center">
-                    <div class="row">
-                        <div class="mb-3 col-lg-6 col-sm-12 mx-auto pt-3">
-                            <label class="form-label fw-bold">Title:</label>
+                    <div class="row px-0">
+                        <div class="mb-5 col-12 mx-auto pt-3">
                             <?php $this->field('title'); ?>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="mb-3 col-lg-6 col-sm-12 mx-auto">
-                            <label class="form-label fw-bold">Url:</label>
-                            <?php $this->field('url'); ?>
+                    <div class="row px-0">
+                        <div class="mb-3 col-12 mx-auto">
+                            <div class="row">
+                                <div class="col-2 d-flex align-items-center">
+                                    <label class="form-label fw-bold mb-0">Url</label>
+                                </div>
+                                <div class="col-10">
+                                    <?php $this->field('url'); ?>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="row g-3 align-items-center m-0">
-                        <?php $this->field('token'); ?>
-                        <input class="form-control rounded-0 border border-1" type="hidden" name="_method" value="<?php echo $this->id ? 'PUT' : 'POST' ?>">
-                        <div class="col-xl-6 col-sm-6 text-end">
-                            <a href="<?php echo $this->link_list ?>">
-                                <button type="button" class="btn btn-outline-secondary">Cancel</button>
-                            </a>
-                        </div>
-                        <div class="col-xl-3 col-sm-6 text-start ">
-                            <button type="submit" class="btn btn-outline-success">Save</button>
+                        <div class="modal-footer">
+                            <?php $this->field('token'); ?>
+                            <input class="form-control rounded-0 border border-1" id="task" type="hidden" name="_method" value="POST">
+                            <div class="row">
+                                <div class="col-6 text-end pe-0">
+                                    <a href="<?php echo $this->link_list ?>">
+                                        <button type="button" class="btn btn-outline-secondary fs-4">Cancel</button>
+                                    </a>
+                                </div>
+                                <div class="col-6 text-end pe-0 ">
+                                    <button type="submit" class="btn btn-outline-success fs-4">Save</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </form>
-
         </div>
     </div>
 </div>
