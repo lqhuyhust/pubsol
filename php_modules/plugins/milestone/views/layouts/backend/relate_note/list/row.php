8389362ba48c1720b103ae20c7dd486c87e4a730
Nguyễn Đắc Hùng <641043@sv.vnua.edu.vn>
2022-12-07 10:27:38 +0700

    fix button detail and edit

--- php_modules/plugins/milestone/views/layouts/backend/relate_note/list/row.php
+++ php_modules/plugins/milestone/views/layouts/backend/relate_note/list/row.php
@@ -2,10 +2,29 @@
     <td>
         <input class="checkbox-item" type="checkbox" name="ids[]" value="<?php echo $this->item['id']; ?>">
     </td>
-    <td><a href="<?php echo $this->link_form . '/' . $this->item['id']; ?>"><?php echo  $this->item['title']  ?></a></td>
+    <td>
+        <a href="#" class="show_data"
+            data-id="<?php echo  $this->item['id'] ?>" 
+            data-title="<?php echo  $this->item['title']  ?>" 
+            data-description="<?php echo $this->item['description']; ?>" 
+            data-bs-placement="top" 
+            data-bs-toggle="modal" 
+            data-bs-target="#exampleModalToggle">
+            <?php echo  $this->item['title']  ?>
+        </a>
+    </td>
     <td><?php echo   $this->item['description'] ?></td>
     <td>
-        <a class="fs-4 me-1" href="<?php echo $this->link_form . '/' . $this->item['id']; ?>"><i class="fa-solid fa-pen-to-square"></i></a>
+        <a href="#"
+            class="fs-4 me-1 show_data"
+            data-id="<?php echo  $this->item['id'] ?>" 
+            data-title="<?php echo  $this->item['title']  ?>" 
+            data-description="<?php echo $this->item['description']; ?>" 
+            data-bs-placement="top" 
+            data-bs-toggle="modal" 
+            data-bs-target="#exampleModalToggle">
+            <i class="fa-solid fa-pen-to-square"></i>
+        </a>
         <a data-id="<?php echo  $this->item['id'] ?>" style="color:#3b7ddd;" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete" class="delete fs-4 ps-1 border-0 bg-transparent button_delete_item"><i class="fa-solid fa-trash"></i></a>
     </td>
 </tr>
