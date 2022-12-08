ec1aeae639eda8871b23be614eb103b9e7ee58ef
Nguyễn Đắc Hùng <641043@sv.vnua.edu.vn>
2022-12-08 10:58:43 +0700

    delete button update

--- php_modules/plugins/milestone/views/layouts/backend/relate_note/list/row.php
+++ php_modules/plugins/milestone/views/layouts/backend/relate_note/list/row.php
@@ -15,16 +15,6 @@
     </td>
     <td><?php echo   $this->item['description'] ?></td>
     <td>
-        <a href="#"
-            class="fs-4 me-1 show_data"
-            data-id="<?php echo  $this->item['id'] ?>" 
-            data-title="<?php echo  $this->item['title']  ?>" 
-            data-description="<?php echo $this->item['description']; ?>" 
-            data-bs-placement="top" 
-            data-bs-toggle="modal" 
-            data-bs-target="#exampleModalToggle">
-            <i class="fa-solid fa-pen-to-square"></i>
-        </a>
         <a data-id="<?php echo  $this->item['id'] ?>" style="color:#3b7ddd;" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete" class="delete fs-4 ps-1 border-0 bg-transparent button_delete_item"><i class="fa-solid fa-trash"></i></a>
     </td>
 </tr>
