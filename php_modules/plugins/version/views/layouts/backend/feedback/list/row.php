38f4fb7827ff43ccbaf7fb660baa1b74114ebb76
Nguyễn Đắc Hùng <641043@sv.vnua.edu.vn>
2022-12-09 09:04:04 +0700

    add link detail feedback

--- php_modules/plugins/version/views/layouts/backend/feedback/list/row.php
+++ php_modules/plugins/version/views/layouts/backend/feedback/list/row.php
@@ -1,9 +1,6 @@
 <tr>
     <td>
-        <input class="checkbox-item" type="checkbox" name="ids[]" value="<?php echo $this->item['id']; ?>">
-    </td>
-    <td>
-        <a href="#">
+        <a href="<?php echo $this->link_form . '/' . $this->item['id']; ?>">
             <?php echo  $this->item['title']  ?>
         </a>
     </td>
