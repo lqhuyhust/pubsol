455e6f73f2f54af489cb02d8124c7b6248e97441
Nguyễn Đắc Hùng <641043@sv.vnua.edu.vn>
2022-12-09 11:57:57 +0700

    add function detail request

--- php_modules/plugins/milestone/controllers/Request.php
+++ php_modules/plugins/milestone/controllers/Request.php
@@ -35,6 +35,29 @@ class Request extends Admin
         $this->app->set('format', 'html');
     }
 
+    public function detail_request()
+    {
+        $this->isLoggedIn();
+
+        $urlVars = $this->request->get('urlVars');
+        $id = (int) $urlVars['request_id'];
+        
+        $milestone_id = $this->validateMilestoneID();
+        $exist = $this->RequestEntity->findByPK($id);
+
+        if(!empty($id) && !$exist) 
+        {   
+            $this->session->set('flashMsg', "Invalid Request");
+            $this->app->redirect(
+                $this->router->url('admin/requests/'. $milestone_id)
+            );
+        }
+
+        $this->app->set('layout', 'backend.request.detail_request');
+        $this->app->set('page', 'backend');
+        $this->app->set('format', 'html');
+    }
+
     public function list()
     {
         $this->isLoggedIn();
@@ -199,7 +222,8 @@ class Request extends Admin
         $this->isLoggedIn();
 
         $urlVars = $this->request->get('urlVars');
-        $id = (int) $urlVars['milestone_id'];
+        // var_dump($urlVars);die;
+        $id = (int) $urlVars['request_id'];
 
         if(empty($id))
         {
