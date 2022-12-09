2fc56ae208f9a733a43313af159fea67f956a728
Nguyễn Đắc Hùng <641043@sv.vnua.edu.vn>
2022-12-09 11:58:28 +0700

    render layout view detail request

---
+++ php_modules/plugins/milestone/views/layouts/backend/request/detail_request.php
@@ -0,0 +1,5 @@
+
+								<?php echo $this->render('layouts.backend.relate_note.list'); ?>
+								<?php echo $this->render('layouts.backend.document.form'); ?>
+								<?php echo $this->render('layouts.backend.task.list'); ?>
+								<?php echo $this->render('layouts.backend.version_latest.list'); ?>
