b20a38cba279db731786097b34e516d11c8ab1bd
Nguyễn Đắc Hùng <641043@sv.vnua.edu.vn>
2022-12-13 16:15:30 +0700

    fix class, id delete in file js

--- php_modules/plugins/milestone/views/layouts/backend/relate_note/list/javascript.php
+++ php_modules/plugins/milestone/views/layouts/backend/relate_note/list/javascript.php
@@ -6,16 +6,16 @@
     if (window.history.replaceState) {
         window.history.replaceState(null, null, window.location.href);
     }
-    document.getElementById('clear_filter').onclick = function() {
-        document.getElementById("search").value = "";
-        document.getElementById("sort").value = "title asc";
-        document.getElementById('filter_form').submit();
-    };
+    // document.getElementById('clear_filter').onclick = function() {
+    //     document.getElementById("search").value = "";
+    //     document.getElementById("sort").value = "title asc";
+    //     document.getElementById('filter_form').submit();
+    // };
     $(document).ready(function() {
-        $("#select_all").click( function(){
+        $("#select_all_relate_note").click( function(){
             $('.checkbox-item').prop('checked', this.checked);
         });
-        $(".button_delete_item").click(function() {
+        $(".button_delete_item_relate_note").click(function() {
             var id = $(this).data('id');
             var result = confirm("You are going to delete 1 record(s). Are you sure ?");
             if (result) {
@@ -27,7 +27,7 @@
                 return false;
             }
         });
-        $('#delete_selected').click(function(){
+        $('#delete_relate_note_selected').click(function(){
             var count = 0;
             $('input[name="ids[]"]:checked').each(function() {
                 count++;
@@ -44,14 +44,14 @@
         $('#limit').on("change", function (e) {
             $('#filter_form').submit()
         });
-        $(".show_data").click(function() {
+        $(".show_data_relate_note").click(function() {
             var id = $(this).data('id');
             var title = $(this).data('title');
             var description = $(this).data('description');
 
             $('#title').val(title);
             $('#description').val(description);
-
+            
             $('#form_relate_note').attr('action', '<?php echo $this->link_form;?>/' + id);
             if(id) {
                 $('#relate_note').val('PUT');
