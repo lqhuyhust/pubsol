505bdb39168941b2592069e710a55ebb7003becb
Nguyễn Đắc Hùng <641043@sv.vnua.edu.vn>
2022-12-13 17:37:00 +0700

    delete filter

--- php_modules/plugins/milestone/views/layouts/backend/task/list/filter.php
+++ php_modules/plugins/milestone/views/layouts/backend/task/list/filter.php
@@ -11,7 +11,7 @@
                             class="align-middle btn border border-1 show_data" 
                             data-bs-placement="top" 
                             data-bs-toggle="modal" 
-                            data-bs-target="#exampleModalToggle">
+                            data-bs-target="#Popup_form_task">
                             <i class="fa-solid fa-plus"></i>
                         </button>
                     </div>
@@ -26,18 +26,6 @@
                     </div>
                 </div>
             </div>
-            <div class="pe-2">
-                <?php $this->field('sort');  ?>
-            </div>
-            <div class="pe-2">
-                <?php $this->field('search');  ?>
-            </div>
-            <button type='Submit' data-bs-toggle="tooltip" title="Filter" class=" align-middle btn border border-1 ms-2" type="button">
-                <i class="fa-solid fa-filter"></i>
-            </button>
-            <button data-bs-toggle="tooltip" title="Clear Filter" id="clear_filter" class="align-middle btn border border-1 ms-2" type="button">
-                <i class="fa-solid fa-filter-circle-xmark"></i>
-            </button>
         </div>
     </div>
     <div class="col-lg-1 col-sm-12 text-end pe-0 pb-1 ">
