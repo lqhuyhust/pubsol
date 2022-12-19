8190ac46087c88481260e08e53ccad98279cdffd
joomaio.tinhcv <cvtinh2000@gmail.com>
2022-12-19 11:03:08 +0700

    update sidebar icon link

--- php_modules/themes/sdm/widgets/backend/sidebar.php
+++ php_modules/themes/sdm/widgets/backend/sidebar.php
@@ -1,6 +1,6 @@
 <nav id="sidebar" class="sidebar js-sidebar">
     <div class="sidebar-content js-simplebar">
-        <a class="sidebar-brand" href="<?php echo $this->url . '/admin'; ?>">
+        <a class="sidebar-brand" href="<?php echo $this->url; ?>">
             <svg class="sidebar-brand-icon align-middle" width="32px" height="32px" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="miter" color="#FFFFFF" style="margin-left: -3px">
                 <path d="M12 4L20 8.00004L12 12L4 8.00004L12 4Z"></path>
                 <path d="M20 12L12 16L4 12"></path>
