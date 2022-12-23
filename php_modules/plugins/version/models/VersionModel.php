50209ca2e5169a785ec2c0680e928a1ea97f299f
joomaio.tinhcv <cvtinh2000@gmail.com>
2022-12-23 08:46:48 +0700

    update version get number

--- php_modules/plugins/version/models/VersionModel.php
+++ php_modules/plugins/version/models/VersionModel.php
@@ -16,37 +16,29 @@ class VersionModel extends Base
 { 
     public function getVersion()
     {
-        $where = [];
-        $result = $this->VersionEntity->list(0, 0, $where, '');
-        if($result) {
-            foreach($result as $item){
-                $record [] = $item['id'];
-            }
-            $max_id = max($record);
-            $where = [
-                'id = '. $max_id
-            ];
-        }
-        $result = $this->VersionEntity->list(0, 0, $where, '');
-        foreach($result as $item){}
+        $version_x = $this->OptionModel->get('version_format_x', 'x');
+        $version_y = $this->OptionModel->get('version_format_y', 'y');
+        $version_z = $this->OptionModel->get('version_format_z', 'z');
 
-        $current_version = $item['version'];
-        if($current_version == '')
-        {
-            $current_version = '0.0.0';
-        }
+        $version_lastest = $this->VersionEntity->list(0, 1, [], 'id desc');
+        $current_version = $version_lastest ? $version_lastest[0]['version'] : '';
+        
         $vParts = explode('.', $current_version);
+        $maxMajor = (int) str_repeat('9', substr_count('x', $version_x));
+        $maxMinor = (int) str_repeat('9', substr_count('x', $version_y));
+        $maxPatch = (int) str_repeat('9', substr_count('x', $version_z));
+
         $partsArray = [
             'major' => $vParts[0],
             'minor' => $vParts[1],
             'patch' => $vParts[2],
         ];
         $partsArray['patch'] = $partsArray['patch'] + 1;
-        if ($partsArray['patch'] > 99) {
+        if ($partsArray['patch'] > $maxPatch) {
             $partsArray['minor'] = $partsArray['minor'] + 1;
             $partsArray['patch'] = 0;
         }
-        if ($partsArray['minor'] > 99) {
+        if ($partsArray['minor'] > $maxMinor) {
             $partsArray['major'] = $partsArray['major'] + 1;
             $partsArray['minor'] = 0;
         }
