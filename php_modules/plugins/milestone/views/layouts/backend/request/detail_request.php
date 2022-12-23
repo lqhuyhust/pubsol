6d186a71ba663b806fc0dd9a2b7956262738075e
joomaio.tinhcv <cvtinh2000@gmail.com>
2022-12-23 14:48:16 +0700

    fix view form popup

--- php_modules/plugins/milestone/views/layouts/backend/request/detail_request.php
+++ php_modules/plugins/milestone/views/layouts/backend/request/detail_request.php
@@ -70,31 +70,29 @@
     <div class="modal-dialog modal-dialog-centered " style="max-width: 600px;">
         <div class="modal-content container px-5 pt-5">
             <form action="<?php echo $this->link_form_request;?>" method="post" id="form_request">
-				<div class="row g-3 align-items-center">
-                    <div class="row px-0">
-                        <div class="mb-3 col-12 mx-auto pt-3">
-                            <input name="title" type="text" id="title" required="" placeholder="Request" value="<?php echo $this->request['title'];?>" class="form-control h-50-px fw-bold rounded-0 fs-3">
-                        </div>
+                <div class="row">
+                    <div class="mb-3 col-12 mx-auto pt-3">
+                        <input name="title" type="text" id="title" required="" placeholder="Request" value="<?php echo $this->request['title'];?>" class="form-control h-50-px fw-bold rounded-0 fs-3">
                     </div>
-					<input type="hidden" name="detail_request" value="1">
-                    <div class="row px-0 mb-3">
-                        <div class="col-12 d-flex align-items-center">
-                            <label class="form-label fw-bold mb-2">Description</label>
-                        </div>
-                        <div class="col-12">
-                            <textarea name="description" type="text" id="description" placeholder="Enter description" class="form-control rounded-0 border border-1 py-1 fs-4-5"><?php echo $this->request['description'];?></textarea>                        
-						</div>
+                </div>
+                <input type="hidden" name="detail_request" value="1">
+                <div class="row mb-3">
+                    <div class="col-12 d-flex align-items-center">
+                        <label class="form-label fw-bold mb-2">Description</label>
                     </div>
-                    <div class="row g-3 align-items-center m-0">
-                        <div class="modal-footer">
-                            <input name="token" type="hidden" id="token" value="91e0f6584395a6a937615717605e92c7">                            <input class="form-control rounded-0 border border-1" id="request" type="hidden" name="_method" value="PUT">
-                            <div class="row">
-                                <div class="col-6 text-end pe-0">
-                                    <button type="button" class="btn btn-outline-secondary fs-4" data-bs-dismiss="modal">Cancel</button>
-                                </div>
-                                <div class="col-6 text-end pe-0 ">
-                                    <button type="submit" class="btn btn-outline-success fs-4">Save</button>
-                                </div>
+                    <div class="col-12">
+                        <textarea name="description" type="text" id="description" placeholder="Enter description" class="form-control rounded-0 border border-1 py-1 fs-4-5"><?php echo $this->request['description'];?></textarea>                        
+                    </div>
+                </div>
+                <div class="row g-3 align-items-center m-0">
+                    <div class="modal-footer">
+                        <input name="token" type="hidden" id="token" value="91e0f6584395a6a937615717605e92c7">                            <input class="form-control rounded-0 border border-1" id="request" type="hidden" name="_method" value="PUT">
+                        <div class="row">
+                            <div class="col-6 text-end pe-0">
+                                <button type="button" class="btn btn-outline-secondary fs-4" data-bs-dismiss="modal">Cancel</button>
+                            </div>
+                            <div class="col-6 text-end pe-0 ">
+                                <button type="submit" class="btn btn-outline-success fs-4">Save</button>
                             </div>
                         </div>
                     </div>
