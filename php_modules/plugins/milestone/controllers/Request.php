54c7eacf8bbf0f9ac1fab5a76f442a43f5c094c9
Nguyễn Đắc Hùng <641043@sv.vnua.edu.vn>
2023-03-08 16:21:53 +0700

    update notice create request trước khi create version

--- php_modules/plugins/milestone/controllers/Request.php
+++ php_modules/plugins/milestone/controllers/Request.php
@@ -91,7 +91,7 @@ class Request extends Admin
         // TODO: validate new add
         $newId =  $this->RequestEntity->add([
             'milestone_id' => $milestone_id,
-            'version_id' => $version_latest['id'],
+            'version_id' => $version_latest ? $version_latest['version'] : 0,
             'title' => $title,
             'description' => $description,
             'created_by' => $this->user->get('id'),
@@ -110,7 +110,11 @@ class Request extends Admin
         }
         else
         {
-            $this->session->set('flashMsg', 'Create Successfully!');
+            if(!$version_latest){
+                $this->session->set('flashMsg', 'Create Request Successfully! Please create version first');
+            } else {
+                $this->session->set('flashMsg', 'Create Successfully!');
+            }
             return $this->app->redirect(
                 $this->router->url('requests/'. $milestone_id)
             );
