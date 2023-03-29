4ac13211087a8177996e728ead4e5d5a00e3a523
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-03-29 19:25:42 +0700

    update load entity

---
+++ php_modules/plugins/core/registers/Entity.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\plugins\user\registers;
+
+use SPT\Application\IApp;
+use SPT\Support\Loader;
+
+class Entity
+{
+    public static function loadEntity( IApp $app )
+    {
+        $container = $app->getContainer();
+        foreach(new \DirectoryIterator($app->getPluginPath()) as $item) 
+        {
+            if (!$item->isDot() && $item->isDir()) 
+            { 
+                $path = $app->getPluginPath().$item->getBasename().'/entities';
+                $namespace = $app->getNamespace().'\\plugins\\'.$item->getBasename().'\entities';
+                $inners = Loader::findClass($path, $namespace);
+                foreach($inners as $class)
+                {
+                    if(class_exists($class))
+                    {
+                        $entity = new $class($container->get('query'));
+                        $entity->checkAvailability();
+                        $container->share( $class, $entity, true);
+                        $alias = explode('\\', $class);
+                        $container->alias( $alias[count($alias) - 1], $class);
+                    }
+                }
+        
+            }
+        }
+        
+    }
+}
