ef3a551cd207175c8bb6bb345a1acee2483fe4df
Pham Minh <smpleader@gmail.com>
2023-05-10 18:53:19 +0700

    fix name and typoe

--- php_modules/plugins/core/registers/Bootstrap.php
+++ php_modules/plugins/core/registers/Bootstrap.php
@@ -4,12 +4,16 @@ namespace App\plugins\core\registers;
 use SPT\Query;
 use SPT\Response;
 use SPT\Application\IApp;
+use SPT\Support\Loader;
 use SPT\Extend\Pdo as PdoWrapper;
 use App\plugins\core\libraries\Token;
 use SPT\Session\Instance as Session;
 use SPT\Session\PhpSession;
 use SPT\Session\DatabaseSession;
 use SPT\Session\DatabaseSessionEntity;
+use SPT\User\Instance as UserInstance;
+use SPT\User\SPT\User as UserAdapter;
+use App\plugins\user\entities\UserEntity;
 
 class Bootstrap
 {
