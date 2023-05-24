b7b40a26508e4c97cd47293364518e512d6186fc
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-05-24 14:04:22 +0700

    remove description controller

--- php_modules/plugins/googleauth/controllers/Google.php
+++ php_modules/plugins/googleauth/controllers/Google.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * SPT software - homeController
- * 
- * @project: https://github.com/smpleader/spt
- * @author: Pham Minh - smpleader
- * @description: Just a basic controller
- * 
- */
-
 namespace App\plugins\googleauth\controllers;
 use SPT\Web\MVVM\ControllerContainer as Controller;
 
