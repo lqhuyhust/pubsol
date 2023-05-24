87b9141bab55a0a7dcb828512937ab780fa40007
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-05-24 11:01:59 +0700

    check permission calender

--- php_modules/plugins/calendar/controllers/Calendar.php
+++ php_modules/plugins/calendar/controllers/Calendar.php
@@ -12,12 +12,10 @@ namespace App\plugins\calendar\controllers;
 
 use SPT\Web\MVVM\ControllerContainer as Controller;
 
-class Calendar extends Admin 
+class Calendar extends Controller 
 {
     public function diagram()
     {
-        $this->isLoggedIn();
-
         $this->app->set('layout', 'backend.calendar.diagram');
         $this->app->set('page', 'backend');
         $this->app->set('format', 'html');
