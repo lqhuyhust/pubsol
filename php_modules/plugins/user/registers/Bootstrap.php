b7f27f4793ecd900bb01056e178c1bf2536e7b76
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-05-25 16:21:14 +0700

    update check permission

--- php_modules/plugins/user/registers/Bootstrap.php
+++ php_modules/plugins/user/registers/Bootstrap.php
@@ -2,16 +2,11 @@
 namespace App\plugins\user\registers;
 
 use SPT\Application\IApp;
-use App\plugins\user\libraries\Permission;
 
 class Bootstrap
 {
     public static function initialize( IApp $app)
     {
         // TODO: move user setting here
-
-        // Register permission librarie
-        $container = $app->getContainer();
-        $container->set('permission', new Permission($app));
     }
 }
