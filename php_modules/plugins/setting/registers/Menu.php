a3c28f0b365c30b9b756bd65c115f163e07b4fd2
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-05-25 10:53:54 +0700

    update menu setting

--- php_modules/plugins/setting/registers/Menu.php
+++ php_modules/plugins/setting/registers/Menu.php
@@ -6,20 +6,32 @@ use SPT\Support\Loader;
 
 class Menu
 {
-    public static function registerMenu( IApp $app )
+    public static function registerItem( IApp $app )
     {
         $container = $app->getContainer();
-        $menu_root = $container->exists('menu') ? $container->get('menu') : [];
+        $router = $container->get('router');
+        $path_current = $router->get('actualPath');
         $permission = $container->exists('permission') ? $container->get('permission') : null;
         $allow = $permission ? $permission->checkPermission(['setting_manager']) : true;
+
         if (!$allow)
         {
             return false;
         }
 
-        $menu[] = [['settings', 'settings',], 'settings', 'Settings', '<i class="fa-solid fa-gear"></i>', []];
+        $active = strpos($path_current, 'settings') !== false ? 'active' : '';
+        $menu = [
+            [
+                'link' => $router->url('settings'),
+                'title' => 'Settings', 
+                'icon' => '<i class="fa-solid fa-gear"></i>',
+                'class' => $active,
+            ]
+        ];
 
-        $menu_root[10] = isset($menu_root[10]) ? array_merge($menu_root[10], $menu) : $menu;
-        $container->set('menu', $menu_root);
+        return [
+            'menu' => $menu,
+            'order' => 10,
+        ];
     }
 }
