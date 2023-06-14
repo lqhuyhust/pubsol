861d667ffee38e074ac0b4cce7b1204103cab440
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-06-14 16:25:40 +0700

    refactor model report

--- php_modules/plugins/report/models/ReportModel.php
+++ php_modules/plugins/report/models/ReportModel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * SPT software - Model
  * 
@@ -13,20 +14,67 @@ namespace App\plugins\report\models;
 use SPT\Container\Client as Base;
 
 class ReportModel extends Base
-{ 
+{
     // Write your code here
     public function getTypes()
     {
         $app = $this->container->get('app');
         $types = [];
-        $app->plgLoad('report', 'registerType', function($type) use (&$types)
-        {
-            if (is_array($type) && $type)
-            {
+        $app->plgLoad('report', 'registerType', function ($type) use (&$types) {
+            if (is_array($type) && $type) {
                 $types = array_merge($type, $types);
             }
         });
 
         return $types;
+    }
+
+    public function update($data)
+    {
+        if (!$data || !is_array($data) || !$data['id']) {
+            return false;
+        }
+
+        $try = $this->DiagramEntity->update([
+            'id' => $id,
+            'status' => $find['status'] ? 0 : 1,
+        ]);
+
+        return $try;
+    }
+
+    public function remove($id)
+    {
+        if (!$id) {
+            return false;
+        }
+
+        $types = $this->getTypes();
+        $find = $this->DiagramEntity->findByPK($id);
+        if ($find) 
+        {
+            $type = isset($types[$find['report_type']]) ? $types[$find['report_type']] : [];
+        }
+
+        if (isset($type['remove_object'])) {
+            $remove_object = $this->container->get($type['remove_object']);
+        }
+
+        if (is_object($remove_object)) 
+        {
+            if ($remove_object->remove($id)) 
+            {
+                return true;
+            }
+        } 
+        else 
+        {
+            if ($this->DiagramEntity->remove($id)) 
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
