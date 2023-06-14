66932ce90c12a0230197dd9d002b23b6bbdef992
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-06-14 14:21:07 +0700

    update model milestone

--- php_modules/plugins/milestone/models/MilestoneModel.php
+++ php_modules/plugins/milestone/models/MilestoneModel.php
@@ -28,4 +28,79 @@ class MilestoneModel extends Base
         }
         return $try;
     }   
+
+    public function validate($data)
+    {
+        if (!$data || !is_array($data))
+        {
+            return false;
+        }
+
+        if (!$data['title']) 
+        {
+            $this->session->set('flashMsg', 'Error: Title can\'t empty! ');
+            return false;
+        }
+
+        $where = ['title = "' . $title . '"'];
+        if (isset($data['id']) && $data['id'])
+        {
+            $where[] = 'id <> '. $data['id'];
+        }
+        $findOne = $this->MilestoneEntity->findOne($where);
+        if ($findOne) {
+            $this->session->set('flashMsg', 'Error: Title is already in use! ');
+            return false;
+        }
+
+        if ($data['start_date'] == '')
+            $data['start_date'] = NULL;
+        if ($data['end_date'] == '')
+            $data['end_date'] = NULL;
+
+        return true;
+    }
+
+    public function add($data)
+    {
+        if (!$data || !is_array($data))
+        {
+            return false;
+        }
+
+        $newId =  $this->MilestoneEntity->add([
+            'title' => $data['title'],
+            'description' => $data['description'],
+            'start_date' => $data['start_date'],
+            'end_date' => $data['end_date'],
+            'status' => $data['status'],
+            'created_by' => $this->user->get('id'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'modified_by' => $this->user->get('id'),
+            'modified_at' => date('Y-m-d H:i:s')
+        ]);
+
+        return $newId;
+    }
+
+    public function update($data)
+    {
+        if (!$data || !is_array($data))
+        {
+            return false;
+        }
+
+        $try = $this->MilestoneEntity->update([
+            'title' => $data['title'],
+            'description' => $data['description'],
+            'start_date' => $data['start_date'],
+            'end_date' => $data['end_date'],
+            'status' => $data['status'],
+            'modified_by' => $this->user->get('id'),
+            'modified_at' => date('Y-m-d H:i:s'),
+            'id' => $ids,
+        ]);
+
+        return $try;
+    }
 }
