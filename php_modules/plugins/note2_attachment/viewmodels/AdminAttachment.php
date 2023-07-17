04475061816381a61b944801d925e50717786bd4
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-07-17 11:58:26 +0700

    update vm attachment

--- php_modules/plugins/note2_attachment/viewmodels/AdminAttachment.php
+++ php_modules/plugins/note2_attachment/viewmodels/AdminAttachment.php
@@ -14,7 +14,7 @@ namespace App\plugins\note2_attachment\viewmodels;
 use SPT\Web\ViewModel;
 use SPT\Web\Gui\Form;
 
-class AdminNote extends ViewModel
+class AdminAttachment extends ViewModel
 {
     public static function register()
     {
@@ -25,62 +25,11 @@ class AdminNote extends ViewModel
         ];
     }
     
-    private function getItem()
+    private function attachments($layoutData, $viewData)
     {
-        $urlVars = $this->request->get('urlVars');
-        $id = $urlVars && isset($urlVars['id']) ? (int) $urlVars['id'] : 0;
-
-        $data = $id ? $this->NoteAttachment->getDetail($id) : [];
+        $id = $viewData['id'] ? $viewData['id'] : 0;
+        $this->NoteAttachmentModel->getData($id);
 
         return $data;
-    }
-
-    public function form()
-    {
-        $data = $this->getItem();
-        $id = isset($data['id']) ? $data['id'] : 0;
-
-        $form = new Form($this->getFormFields(), $data);
-        
-        return [
-            'id' => $id,
-            'form' => $form,
-            'data' => $data,
-            'title_page_edit' => $data && $data['title'] ? $data['title'] : 'New Note',
-            'url' => $this->router->url(),
-            'link_list' => $this->router->url('note2'),
-            'link_form' => $id ? $this->router->url('note2/detail') : $this->router->url('new-note2/file'),
-        ];
-        
-    }
-
-    public function getFormFields()
-    {
-        $fields = [
-            'notice' => [
-                'textarea',
-                'label' => 'Notice',
-                'placeholder' => 'Notice',
-                'formClass' => 'form-control',
-            ],
-            'file' => [
-                'file',
-                'label' => 'File',
-                'required' => 'required',
-                'formClass' => 'form-control',
-            ],
-            'title' => [
-                'text',
-                'showLabel' => false,
-                'placeholder' => 'New Title',
-                'formClass' => 'form-control border-0 border-bottom fs-2 py-0',
-                'required' => 'required',
-            ],
-            'token' => ['hidden',
-                'default' => $this->token->value(),
-            ],
-        ];
-
-        return $fields;
     }
 }
