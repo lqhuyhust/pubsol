d33ea082dfb8d199bbf8576e55cc47f9dd85b222
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-08-03 13:46:21 +0700

    update test structor

--- tests/ready/DBConnectTest.php
+++ tests/ready/DBConnectTest.php
@@ -4,6 +4,7 @@ namespace Tests\ready;
 use Tests\Test as TestCase;
 use SPT\Application\Configuration;
 use SPT\Extend\Pdo as PdoWrapper;
+use SPT\Support\Loader;
 
 class DBConnectTest extends TestCase
 {
@@ -23,6 +24,29 @@ class DBConnectTest extends TestCase
         }
 
         $this->assertTrue($try);
+    }
+
+    public function testEntity()
+    {
+        $app = $this->prepareApp();
+        $container = $app->getContainer();
+        $plgList = $app->plugin(true);
+        
+        foreach($plgList as $plg)
+        {
+            Loader::findClass( 
+                $plg['path']. '/entities', 
+                $plg['namespace']. '\entities', 
+                function($classname, $fullname) use ($container, &$entities)
+                {
+                    if ($container->exists($classname))
+                    {
+                        $entity = $container->get($classname);
+                        $try = $entity->checkAvailability();
+                        $this->assertNotFalse($try);
+                    }
+            });
+        }
     }
 
 }
